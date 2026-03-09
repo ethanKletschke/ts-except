3459a5c70c6b14b3cc67285b8001185f20946101
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-09 09:24:41 +0200

    docs(src): Add TSDoc comments to ValidationError

--- src/Validation/ValidationError.ts
+++ src/Validation/ValidationError.ts
@@ -1,3 +1,13 @@
+/**
+ * A simple validation error class.
+ *
+ * @remarks
+ * This class adds no basic functionality to the standard
+ * `Error` class yet.
+ *
+ * @alpha
+ * @experimental
+ */
 export default class ValidationError extends Error {
   constructor(msg: string) {
     // Call the Error constructor to pass the message to the base Error class
@@ -6,4 +16,6 @@ export default class ValidationError extends Error {
     // Set the error's name for error output
     this.name = "ValidationError";
   }
+
+  // TODO -> Add overridden toString method
 }
