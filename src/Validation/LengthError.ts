8974d7878956fef89961f4332358b5fadf000c16
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-09 09:09:17 +0200

    docs(src): Add LengthError TSDoc comment

--- src/Validation/LengthError.ts
+++ src/Validation/LengthError.ts
@@ -1,8 +1,31 @@
 import ValidationError from "./ValidationError";
 
 /**
+ * Thrown when a string's length doesn't match a given length.
  *
  * @remarks
+ * The `LengthError` class, which extends the `ValidationError`
+ * class, is intended to be thrown when a string's length does not
+ * match that of a given length.
+ *
+ * @example
+ * ```js
+ * // Hypothetical import statement, subject to change
+ * import { LengthError } from "ts-except";
+ *
+ * let x: string = "123456";
+ *
+ * try {
+ *   if (x.length !== 6) {
+ *     throw new LengthError("Length of X not equal to 6");
+ *   }
+ * } catch (ex) {
+ *   if (ex instanceof LengthError) {
+ *     console.error(ex.message);
+ *     process.exit(1);
+ *   }
+ * }
+ * ```
  *
  */
 export default class LengthError extends ValidationError {
