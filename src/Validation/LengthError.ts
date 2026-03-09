aee764b5a9e648a48b6778885f91c78c2934c204
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-09 08:44:52 +0200

    feat(src): Remove redundant "_maxlen" property

--- src/Validation/LengthError.ts
+++ src/Validation/LengthError.ts
@@ -1,19 +1,15 @@
 import ValidationError from "./ValidationError";
 
 export default class LengthError extends ValidationError {
-  constructor(msg: string, private _maxLen: number) {
+  constructor(msg: string) {
     super(msg);
 
     this.name = "LengthError";
   }
 
-  get maxLength() {
-    return this._maxLen;
-  }
-
   static checkLen(toTest: string, maxLength: number) {
     if (toTest.length > maxLength) {
-      throw new LengthError(`Length of "${toTest}" is greater than ${maxLength}`, maxLength);
+      throw new LengthError(`Length of string "${toTest}" is greater than ${maxLength}`);
     }
   }
 }
