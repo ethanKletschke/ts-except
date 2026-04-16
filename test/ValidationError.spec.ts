2c2f3b79b98e26200dcecdf97966713188e690de
Ethan Kletschke <bbruhingson@gmail.com>
2026-04-16 11:38:56 +0200

    test: Update `ValidationError` unit tests

--- test/ValidationError.spec.ts
+++ test/ValidationError.spec.ts
@@ -4,23 +4,27 @@ import { expect } from "chai";
 describe("ValidationError", function () {
   // Test constructor logic
   describe("constructor()", function () {
-    it("should create an instance of ValidationError", function () {
+    it("Creates an instance of ValidationError", function () {
       expect(new ValidationError("Invalid Input")).to.be.instanceOf(
         ValidationError,
         "Incorrect instance generated."
       );
     });
 
-    it("should set the error message correctly", function () {
+    it("Sets the error message correctly", function () {
       expect(new ValidationError("Invalid Input").message).to.equal(
         "Invalid Input",
         "Incorrect assignment of ValidationError message."
       );
     });
+
+    it("Inherits from \"Error\"", function () {
+      expect(new ValidationError("Invalid Input")).to.be.instanceOf(Error);
+    });
   });
 
   describe("Throwing", function () {
-    it("should be throwable and catchable", function () {
+    it("Is throwable and catchable", function () {
       let caught = false;
 
       try {
